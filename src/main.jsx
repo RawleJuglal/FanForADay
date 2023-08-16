e37b6da72a1c675d8525153c8d7ca51ef5c5c37f
Rawle Juglal <rawledjuglal@gmail.com>
2023-08-15 20:53:17 -0500

    css styling
    added home content and some styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './components/App/App'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
 import './index.css'
+import ffadLogo from './assets/images/FFAD-LOGO.png'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter> 
-)
+const App = ()=>{
+  return(
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          <img src={ffadLogo} alt="logo of a stadium" />
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+    
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="about" element={<About />} />
+      </Routes> 
+    </BrowserRouter> 
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(<App />)
+

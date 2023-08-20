f86e4f6c12a941d1e370289676b6dd0b5e75c1f4
Rawle Juglal <rawledjuglal@gmail.com>
2023-08-19 22:32:23 -0500

    styling
    completed styles for Home
    next step complete styles for About

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
+import fanJumbo from '../../assets/images/fan-jumbo.jpg'
 
 
 const Home = ()=>{
+  const homeStyles = {
+    backgroundImage: `url(${fanJumbo})`, 
+    backgroundSize:'cover', 
+    backgroundRepeat:'no-repeat'
+  }
+
   return(
-    <div className="home-container">
+    <div className="home-container flex flex-column flex-center" style={homeStyles}>
         <h1>You got the travel plans, we got the travel vans.</h1>
         <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
         <Link to="vans">Find your van</Link>
